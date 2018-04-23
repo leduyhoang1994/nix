c9de3c8e6ad635dbc80253c0f8e6e6801800b80f
leduyhoang1994 <leduyhoang1994@gmail.com>
2018-04-23 17:12:08 +0700

    done

--- wp-content/themes/kutetheme/functions.php
+++ wp-content/themes/kutetheme/functions.php
@@ -692,9 +692,10 @@ function recursive_array_search_php_91365( $needle, $haystack )
 }
 
 function remove_menus() {
+    $hideRole = "shop_manager";
     $userInfo = get_userdata(get_current_user_id());
     $userRoles = $userInfo->roles;
-    var_dump($userRoles);
+    
     if (in_array($hideRole, $userRoles)) {
         remove_menu_page( 'edit.php?post_type=testimonial' );    //Pages  te
         remove_menu_page( 'edit.php?post_type=look-books' );    //Pages  te
